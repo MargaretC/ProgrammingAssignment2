f853616d023c444f0e764c86bd75b9727cb407ff
MargaretC <margaret_cottam@yahoo.com>
2014-11-23 12:43:55 -0600

    Programming Assignment # 2
    functions to compute a matrix inverse and to store the matrix and its
    inverse to avoid unnecessary computations.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains two functions:
+##
+## makeCacheMatrix takes a matrix as an argument
+##  it creates a list made up of functions to store and
+##  retrieve the matrix and its inverse.
+##
+## cacheSolve computes the inverse of a matrix.  If the
+##  matrix and its inverse have been previously stored
+##  cacheSolve uses the functions of makeCacheMatrix to 
+##  retrieve and return the stored values.
+##  If this is a new matrix, cacheSolve computes the
+##  inverse and calls the functions of makeCacheMatrix 
+##  to store the values
+## 
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a structure to store the inverse
+## of matrix x
+##  function getMatrix retrieves the stored inverse
+##  function setMatrix computes and stores the inverse
+##  function set stores matrix x and sets the inverse to null
+##     this initializes the values
+##  function get retieves the stored matrix x
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  xInv<-NULL  ## initialize
+  set<-function(y) {
+    x<<-y
+    xInv<<-NULL
+  }
+  get<-function() x
+  setMatrix <-function(solve) xInv<<-solve
+  getMatrix <-function() xInv
+  list(set = set,get = get, setMatrix = setMatrix, getMatrix = getMatrix)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve checks to see if a matrix has been stored. If so
+## return the stored inverse.  Otherwise, compute the inverse,
+#  store the inverse and return the new matrix.
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x) {
         ## Return a matrix that is the inverse of 'x'
+  xInv<-x$getMatrix()  ## retrive matrix stored by makeCacheMatrix
+  
+##  if xInv is not null, inverse has been cached.  Just 
+##   return it
+
+  if (!is.null(xInv)) {
+    message("getting cached data")
+    return(xInv) 
+  }
+##  Inverse must be computed.  use get to get the original 
+##  matrix, solve to compute the inverse, then store the inverse
+##  and display it.
+
+  else {
+     data<-x$get()
+     xInv<-solve(data)
+     x$setMatrix(xInv)
+     xInv
+  }
+    
 }
